ce82927a56d2d3e5a8a0168803e61849158e9d38
Felpis3196 <126421629+Felpis3196@users.noreply.github.com>
2023-11-02 18:50:32 -0300

    Update ItemVendasController.cs

--- SimasTurbo/Controllers/ItemVendasController.cs
+++ SimasTurbo/Controllers/ItemVendasController.cs
@@ -12,7 +12,7 @@ using SimasTurbo.Models;
 
 namespace SimasTurbo.Controllers
 {
-    //[Authorize(Roles = "Admin,Operador")]
+    [Authorize(Roles = "Admin,Operador")]
     public class ItemVendasController : Controller
     {
         private readonly SimasContext _context;
